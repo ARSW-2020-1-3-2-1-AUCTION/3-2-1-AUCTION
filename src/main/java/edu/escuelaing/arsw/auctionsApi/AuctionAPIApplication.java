701467235d956575cc2b99b48fef6ac7045bad8f
Mateo Gonzalez <matteu20@hotmail.com>
2020-03-20 19:33:16 -0500

    saldo

--- src/main/java/edu/escuelaing/arsw/auctionsApi/AuctionAPIApplication.java
+++ src/main/java/edu/escuelaing/arsw/auctionsApi/AuctionAPIApplication.java
@@ -6,17 +6,10 @@ package edu.escuelaing.arsw.auctionsApi;
  * @author david.gonzalez-g
  */
 
-import java.sql.Connection;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
